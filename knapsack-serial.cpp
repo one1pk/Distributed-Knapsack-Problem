6ee00aa9930707876e559af1271975ad67fcf285
pkb13 <pkb13@sfu.ca>
2022-04-05 22:05:00 +0000

    Add some comments to explain the algorithm, add vector cleanup, add file reading code

--- knapsack-serial.cpp
+++ knapsack-serial.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
@@ -31,13 +32,16 @@ void print_items(int n,vector<vector<int>> &dp ,vector <int> &s, vector <int> &v
     }
     cout<<endl;
 }
+
 /*
  * Solution is based on Dynamic Programming Paradigm
  */
 int knapsack_serial(int n, vector<int> &s, vector<int> &v, int S) {
-    // top-down approach
+    // matrix of maximum values obtained after all intermediate combinations of items
     vector<vector<int>> dp(n+1, vector<int>(S+1)); 
-
+    // dp[i][j] is the maximum value that can be obtained by using a subset of the items (i...n−1) (last n−i items) which weighs at most j pounds
+    
+    // top-down approach
     for(int i = n; i >= 0; i--) {
         for(int j = 0; j <= S; j++) {
             if(i==n) {
@@ -45,35 +49,52 @@ int knapsack_serial(int n, vector<int> &s, vector<int> &v, int S) {
                 dp[i][j] = 0; // initial condition
             }
             else {
-                int* choices = new int[2];     
-                choices[0] = dp[i + 1][j];
+                int* choices = new int[2]; 
+                // 1st choice: Don't add item i to knapsack 
+                choices[0] = dp[i + 1][j]; // resuse optimal solution of i+1
                 if(j >= s[i]) {
-                    choices[1] = dp[i+1][j-s[i]]+v[i];
+                    // 2nd choice: Add item i to knapsack
+                    choices[1] = dp[i+1][j-s[i]]+v[i]; // add value of item i to optimal solution of i+1 that weighs at most j-s[i] 
                 }
-                //assuming no negative values
+                // assuming no negative values
                 dp[i][j] = max(choices[0], choices[1]); 
                 delete[] choices;
             }
         }
     }
-    int result = dp[0][S];
     print_items(n,dp,s,v,S);
+
+    int result = dp[0][S]; 
+    dp.~vector();   
     return result;
 }
 
-int main() {
-    vector<int> weights, values;
-    weights = {20, 10, 40, 30};
-    values = {40, 100, 50, 60};
+int main(int argc, char **argv) {
+    if(argc!=2){
+        cout << "Please enter one input file!" << "\n";
+        return 0;
+    }
 
+    ifstream input_file;
+    input_file.open(argv[1], ios::in);
+
+    // decide format of input files and extract data accordingly
+
+    vector<int> weights, values;
     int num_items, capacity;
     num_items = weights.size();
     capacity = DEFAULT_MAX_WEIGHT;
 
+    input_file.close();
+
     printf("Starting knapsack solving...\n"); 
 
-    
-    cout << knapsack_serial(num_items, weights, values, capacity) << "\n";
+    int max_val = knapsack_serial(num_items, weights, values, capacity);
+
+    cout << "Maximum value: " << max_val << endl;
+
+    weights.~vector();
+    values.~vector();
 
     return 0;
 }
